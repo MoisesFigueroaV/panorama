281c7679f75bb20e56057715cb77e25f0ede9d4e
Javier Vega <ja.vegaf@duocuc.cl>
2025-06-24 21:08:40 -0400

    Modificado el archivo de enruitado de las notificaciones

--- back-end/src/modules/notificaciones/notificaciones.routes.ts
+++ back-end/src/modules/notificaciones/notificaciones.routes.ts
@@ -2,6 +2,11 @@ import { Elysia, t } from 'elysia';
 import { obtenerNotificacionesPorUsuario } from './notificaciones.services';
 import { notificacionesResponseSchema } from './notificaciones.types';
 import { CustomError } from '../../utils/errors';
+import { db } from '../../db/drizzle';
+import { notificacionTable } from '../../db/schema/notificaciones.schema';
+import { eq } from 'drizzle-orm';
+import { historialEstadoNotificacionTable } from '../../db/schema/historial_estado_notificacion.schema';
+import { NotificacionConEstado } from './notificaciones.types'
 
 export const notificaciones = new Elysia({ prefix: '/notificaciones', detail: { tags: ['Notificaciones'] } });
 
@@ -13,9 +18,9 @@ notificaciones.get(
       throw new CustomError('ID de usuario inválido', 400);
     }
 
-    const notificacionesDb = await obtenerNotificacionesPorUsuario(id);
+  const notificacionesDb = await obtenerNotificacionesPorUsuario(id) as unknown as NotificacionConEstado[];
 
-    // Ajuste para convertir fechas Date a string ISO antes de devolver:
+
     const notificaciones = notificacionesDb.map(n => ({
       ...n,
       fecha_envio: n.fecha_envio instanceof Date ? n.fecha_envio.toISOString() : n.fecha_envio
@@ -29,3 +34,17 @@ notificaciones.get(
     detail: { summary: 'Obtener notificaciones por usuario', tags: ['Notificaciones'] }
   }
 );
+
+notificaciones.put('/marcar-leida/:id', async ({ params }) => {
+  const id = Number(params.id);
+  if (isNaN(id)) throw new CustomError('ID inválido', 400);
+
+  await db.insert(historialEstadoNotificacionTable).values({
+    id_notificacion: id,
+    id_estado_notificacion: 2, // ID del estado "leída"
+    fecha_cambio: new Date(),
+  });
+
+  return { message: 'Notificación marcada como leída' };
+});
+
